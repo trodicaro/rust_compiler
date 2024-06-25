50ae9fd10fe54d670a8ffbe3c6605bf6b3d4ff1b
R.T <trodicaro@gmail.com>
2024-06-25 15:05:56 -0500

    Add keyword matching

--- rublox/src/tokenize.rs
+++ rublox/src/tokenize.rs
@@ -128,7 +128,26 @@ impl Scanner {
             break;
             }
         }
-        Some(Token::new(IDENTIFIER, &lexeme, 0))
+        let toktype = match lexeme.as_str() {
+            "and" => AND,
+            "class" => CLASS,
+            "else" => ELSE,
+            "false" => FALSE,
+            "for" => FOR,
+            "fun" => FUN,
+            "if" => IF,
+            "nil" => NIL,
+            "or" => OR,
+            "print" => PRINT,
+            "return" => RETURN,
+            "super" => SUPER,
+            "this" => THIS,
+            "true" => TRUE,
+            "var" => VAR,
+            "while" => WHILE,
+            _ => IDENTIFIER
+        };
+        Some(Token::new(toktype, &lexeme, 0))
         } else {
         None
         }
